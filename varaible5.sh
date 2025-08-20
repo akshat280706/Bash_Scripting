af003d65ac6f87ae93e1b0c30bdd14bf0c80d258
akshat <engineeringstudy.2028@gmail.com>
2025-08-20 04:59:54 +0000

    variable

---
+++ varaible5.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+already_there= $(env)
+
+echo"The already declared built-in variables are: "
+echo $already_there
+
+
---
+++ variable5.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+already=$(env)
+echo "The already declared built-in variables are: "
+echo $already
+#lists all already defined variables
+
